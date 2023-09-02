cf8aab8969d475b3928899e6b7a27e0e8e864e87
Ronald Salvador <Zerudosuu@users.noreply.github.com>
2023-09-02 16:20:19 +0800

    adding additional function of memorygame

--- ProjectPractice/main.js
+++ ProjectPractice/main.js
@@ -4,6 +4,14 @@ const resultDisplay = document.querySelector("#result");
 const possibleChoices = document.querySelectorAll("button");
 let userChoice, computerChoice;
 
+//get getting the possiblechoices which composes of buttons, accessed by queryselectorAll
+//loopoing the possiblechoices since there are 3 buttons
+//adding the choice as iterator.
+// arrow to another function, we addeded addevenlistener in the loop for the buttons to have the same functions by clicking.
+// callback arrow function e to determine the button that was clicked.
+// adding userchoice variable with the params e to get the id of the clicked button
+// since we already have what was clicked, the userchoice is the value of userChoiceDisplay
+//using innerHTML to add value inside the html element or tag
 possibleChoices.forEach((choice) =>
   choice.addEventListener("click", (e) => {
     userChoice = e.target.id;
@@ -13,6 +21,10 @@ possibleChoices.forEach((choice) =>
   })
 );
 
+//generateComputerChoice arrowfunction
+//we store the random number generated along with the length of possible choices plus 1 since the lenght will start to 0
+//when random number is equal to 1 or 2 or 3 there are certain string that will be stored in computer choice randomly
+//and it wil be displayed in computerchoiceDisplay
 const generateComputerChoice = () => {
   const randomNumber = Math.floor(Math.random() * possibleChoices.length + 1);
 
@@ -26,6 +38,7 @@ const generateComputerChoice = () => {
   computerChoiceDisplay.innerHTML = computerChoice;
 };
 
+//decision arrow function to check if the computer won, you get the logic of the game
 const Decision = () => {
   if (computerChoice === userChoice) {
     resultDisplay.innerHTML = "Draw!";
