4fcfea54b7486099f59e143303e906dad3783258
Ronald Salvador <Zerudosuu@users.noreply.github.com>
2023-09-02 23:08:40 +0800

    adding day four of JS

--- Dayfour/main.js
+++ Dayfour/main.js
@@ -1,1 +1,35 @@
-console.log("hello world!");
+const notesContainer = document.querySelector(`.notes-container`);
+const createBtn = document.querySelector(`.btn`);
+let note = document.querySelectorAll(`.input-box`);
+
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNotes();
+
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
+createBtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "images/delete.png";
+  notesContainer.appendChild(inputBox).appendChild(img);
+});
+
+notesContainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    updateStorage();
+  } else if (e.target.tagName === "P") {
+    note = document.querySelectorAll(`.input-box`);
+    note.forEach((nt) => {
+      nt.onkeyup = function () {
+        updateStorage();
+      };
+    });
+  }
+});
