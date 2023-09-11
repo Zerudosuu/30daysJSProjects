71cdf73975c19c09fdc4b83813e2150530a9d2a2
Ronald Salvador <Zerudosuu@users.noreply.github.com>
2023-09-11 22:43:51 +0800

    Adding Nav in my Portfolio

---
+++ Portfolio/src/main.js
@@ -0,0 +1,5 @@
+import "../styles/modern-normalize.css";
+import "../styles/style.css";
+import "../styles/Components/head.css";
+import "../styles/Components/hero.css";
+import "../styles/utils.css";
