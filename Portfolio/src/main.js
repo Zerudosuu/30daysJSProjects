3dacc0af9b9806339622562fbda04ec38cc4e946
Ronald Salvador <Zerudosuu@users.noreply.github.com>
2023-09-14 09:15:16 +0800

    partial hero section

--- Portfolio/src/main.js
+++ Portfolio/src/main.js
@@ -2,4 +2,9 @@ import "../styles/modern-normalize.css";
 import "../styles/style.css";
 import "../styles/Components/head.css";
 import "../styles/Components/hero.css";
+import "../styles/Components/mobile-nav.css";
 import "../styles/utils.css";
+
+import mobilenav from "./utils/mobile-nav";
+
+mobilenav();
---
+++ Portfolio/src/utils/mobile-nav.js
@@ -0,0 +1,28 @@
+const mobilenav = () => {
+  const mobile_nav = document.querySelector(".mobile-nav");
+  const headermenubtn = document.querySelector(".header__menu");
+  const closemenu = document.querySelector(".mobile-nav__closebtn");
+  const blob = document.querySelector(".hero__blob");
+
+  headermenubtn.addEventListener("click", () => {
+    mobile_nav.style.display = "flex";
+    console.log("headermenubtn clicked");
+  });
+
+  closemenu.addEventListener("click", () => {
+    mobile_nav.style.display = "none";
+    console.log("closed button was clicked");
+  });
+
+  var rotation = 0;
+  function rotationAnimation() {
+    rotation += 0.5;
+
+    blob.style.transform = "rotate(" + rotation + "deg)";
+    requestAnimationFrame(rotationAnimation);
+  }
+
+  rotationAnimation();
+};
+
+export default mobilenav;
