700b2b77a3bb63729e981e3351aae5417df4cef2
Ronald Salvador <Zerudosuu@users.noreply.github.com>
2023-09-28 22:50:26 +0800

    Restarting Ecam folder

--- ProjectPractice/Exam/main.js
+++ ProjectPractice/Exam/main.js
@@ -1,62 +1,21 @@
-// const container = document.querySelector(`.container`);
-// const playerCharacter = document.createElement("div");
-// playerCharacter.classList.add("character");
-
-// const playerimage = document.createElement("img");
-// playerimage.src = "./images/player1.png";
-// playerCharacter.appendChild(playerimage); // Append the image to the character div
-// container.appendChild(playerCharacter);
-
-// let bulletcontainer = document.createElement("div");
-// bulletcontainer.className = "bullet";
-// bulletcontainer.style.position = "absolute";
-
-// let PlayerPosition = [230, 230, 0, 0];
-// let currentPlayerPosition = PlayerPosition;
-// let xDirection = 2;
-// let yDirection = 2;
-// const stageWidth = 1900;
-// const stageHeight = 900;
-// const userHeight = 100;
-// const userWidth = 100;
-// const bulletWidth = 40;
-
-// function drawUser() {
-//   playerCharacter.style.left = currentPlayerPosition[0] + "px";
-//   playerCharacter.style.bottom = currentPlayerPosition[1] + "px";
-// }
-
-// function moveUser(e) {
-//   switch (e.key) {
-//     case "ArrowLeft":
-//       if (currentPlayerPosition[0] > 0) {
-//         currentPlayerPosition[0] -= 20;
-//         console.log(currentPlayerPosition[0] > 0);
-//         drawUser();
-//       }
-//       break;
-//     case "ArrowRight":
-//       if (currentPlayerPosition[0] < stageWidth - userWidth) {
-//         currentPlayerPosition[0] += 20;
-//         console.log(currentPlayerPosition[0]);
-//         drawUser();
-//       }
-//       break;
-//   }
-// }
-
-// document.addEventListener("keydown", fire);
-
 const playerSlider = document.querySelector("#playerRange");
 const enemySlider = document.querySelector("#enemyRange");
 const playBtn = document.querySelector(".StartRace");
-const Attack = document.querySelector(`.Attack`);
 const playerCharacter = document.querySelector(`.playerImg`);
+const BtnforAttack = document.querySelectorAll(".BtnContainer button");
+const playerSelectionBtns = document.querySelector(".PlayerSelection");
+
+const sample = document.querySelector(".sample");
+
+sample.addEventListener("click", () => {
+  console.log("buttonwasclicked");
+});
 
 //Player Data
 let Player = document.querySelector(".PlayerName");
 let PlayerHealth = document.querySelector("#playerHealthRange");
 let PlayerMana = document.querySelector("#playerManaRange");
+let PlayerImg = document.querySelector(".playerImg");
 
 //Enemy Data
 let Enemy = document.querySelector(".EnemyName");
@@ -72,34 +31,160 @@ let enemyReachedEnd = false;
 const character = [
   {
     Character: "Player",
-    Name: "Ronald",
+    Name: "Bird",
     Health: 100,
     Mana: 100,
+    Attacks: [
+      { Name: "FlameThrower", Damage: 20 },
+      { Name: "Recover", Damage: 0 },
+      { Name: "Peck", Damage: 10 },
+    ],
+    Image: "./images/firebird.png",
   },
-
   {
     Character: "Enemy",
-    Name: "John",
+    Name: "Machine",
     Health: 100,
     Mana: 100,
+    Attacks: [
+      { Name: "GearSlam", Damage: 25 },
+      { Name: "Repair", Damage: 0 },
+      { Name: "Throw", Damage: 15 },
+    ],
+    Image: "./images/robot.png",
+  },
+  {
+    Character: "Player",
+    Name: "Wizard",
+    Health: 80,
+    Mana: 120,
+    Attacks: [
+      { Name: "Fireball", Damage: 30 },
+      { Name: "Heal", Damage: 0 },
+      { Name: "Lightning Bolt", Damage: 35 },
+    ],
+    Image: "./images/wizard.png",
   },
   {
     Character: "Enemy",
-    Name: "William",
-    Health: 100,
+    Name: "Orc",
+    Health: 120,
+    Mana: 0,
+    Attacks: [
+      { Name: "Axe Swing", Damage: 40 },
+      { Name: "Roar", Damage: 0 },
+      { Name: "Stomp", Damage: 20 },
+    ],
+    Image: "./images/orc.png",
+  },
+  {
+    Character: "Player",
+    Name: "Knight",
+    Health: 150,
+    Mana: 50,
+    Attacks: [
+      { Name: "Sword Slash", Damage: 35 },
+      { Name: "Block", Damage: 0 },
+      { Name: "Charge", Damage: 40 },
+    ],
+    Image: "./images/Knight.png",
+  },
+  {
+    Character: "Enemy",
+    Name: "Goblin",
+    Health: 70,
+    Mana: 0,
+    Attacks: [
+      { Name: "Bite", Damage: 15 },
+      { Name: "Hide", Damage: 0 },
+      { Name: "Throw Rocks", Damage: 10 },
+    ],
+    Image: "./images/goblin.png",
+  },
+  {
+    Character: "Player",
+    Name: "Archer",
+    Health: 90,
+    Mana: 80,
+    Attacks: [
+      { Name: "Arrow Shot", Damage: 25 },
+      { Name: "Dodge", Damage: 0 },
+      { Name: "Snipe", Damage: 30 },
+    ],
+    Image: "./images/archer.png",
+  },
+  {
+    Character: "Enemy",
+    Name: "Vampire",
+    Health: 110,
+    Mana: 90,
+    Attacks: [
+      { Name: "Bite", Damage: 25 },
+      { Name: "Bat Form", Damage: 0 },
+      { Name: "Blood Drain", Damage: 20 },
+    ],
+    Image: "./images/Vampire.png",
+  },
+  {
+    Character: "Player",
+    Name: "Ninja",
+    Health: 70,
     Mana: 100,
+    Attacks: [
+      { Name: "Shuriken Throw", Damage: 15 },
+      { Name: "Stealth", Damage: 0 },
+      { Name: "Katana Slash", Damage: 30 },
+    ],
+    Image: "./images/ninja.png",
+  },
+  {
+    Character: "Enemy",
+    Name: "Dragon",
+    Health: 200,
+    Mana: 150,
+    Attacks: [
+      { Name: "Fire Breath", Damage: 50 },
+      { Name: "Wing Strike", Damage: 40 },
+      { Name: "Tail Swipe", Damage: 45 },
+    ],
+    Image: "./images/Dragon.png",
   },
 ];
 
-Player.innerHTML = character[0].Name;
-Enemy.innerHTML = character[1].Name;
-PlayerHealth.value = character[0].Health;
-enemyHealth.value = character[1].Health;
+for (var i = 0; i < 10; i++) {
+  let btn = document.createElement("button");
+  btn.classList.add("playerBtnSelection");
+  btn.id = i;
+  playerSelectionBtns.appendChild(btn);
+}
 
-let healthPLayer = PlayerHealth.value;
-let healthEnemy = enemyHealth.value;
+const SelectionBtn = document.querySelectorAll(".playerBtnSelection");
+const playerChar = document.querySelector(".playerChoosenChar");
 
-let damage = 10;
+Array.from(SelectionBtn).forEach((btn, index) => {
+  let img = document.createElement("img");
+  img.src = character[index].Image;
+  btn.appendChild(img);
+
+  img.addEventListener("click", (e) => {
+    const imageSrc = e.target.src;
+    playerChar.src = imageSrc;
+  });
+});
+
+playerSelectionBtns.addEventListener("click", (e) => {
+  if (e.target.classList.contains("playerBtnSelection")) {
+    console.log(e.target.id);
+    const imageSrc = e.target.querySelector("img").src;
+    playerChar.src = imageSrc;
+  }
+});
+
+BtnforAttack.forEach((btn, index) => {
+  if (character[0].Attacks[index].Name) {
+    btn.innerHTML = character[0].Attacks[index].Name;
+  }
+});
 
 function animatePlayerSlider() {
   if (!enemyReachedEnd) {
@@ -156,83 +241,6 @@ playBtn.addEventListener("click", () => {
   animateEnemySlider();
 });
 
-const playerAttack = () => {
-  const EnemycurrentHealth = (healthEnemy -= damage);
-  enemyHealth.value = EnemycurrentHealth;
-};
+const playerAttack = () => {};
 
-const enemyAttack = () => {
-  const PlayercurrentHealth = (healthPLayer -= damage);
-  PlayerHealth.value = PlayercurrentHealth;
-};
-
-// const canvas = document.querySelector(".playerCanvas");
-// const ctx = canvas.getContext("2d");
-// const myimg = document.querySelector(".myImg");
-
-// function playerAnimate() {
-//   ctx.drawImage(myimg, 10, 30, myImg.width / 2, myimg.height / 2);
-// }
-
-// playerAnimate();
-// const canvas = document.querySelector(".playerCanvas");
-// const ctx = canvas.getContext("2d");
-
-// const canvasWidth = (canvas.width = 778);
-// const canvasHeight = (canvas.height = 625);
-// const spriteWidth = 1038;
-// const spriteHeight = 833;
-// let gameFrame = 0;
-// let stagger = 0;
-// const staggerFrame = 10;
-// let index = 1;
-// let playerState = "run";
-// const myimg = document.querySelector(".myImg");
-
-// const animationStates = [
-//   { name: "run", frames: 4 },
-//   { name: "attack", frames: 6 },
-//   { name: "jump", frames: 9 },
-//   { name: "dash", frames: 7 },
-// ];
-
-// const spriteAnimations = ["run", "attack", "jump", "dash"];
-
-// function animate() {
-//   if (stagger % staggerFrame == 0) {
-//     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-//     index = spriteAnimations.indexOf(playerState);
-//     let frameX = spriteWidth * (gameFrame % animationStates[index].frames);
-//     let frameY = spriteHeight * index;
-//     ctx.drawImage(
-//       myimg,
-//       frameX,
-//       frameY,
-//       spriteWidth,
-//       spriteHeight,
-//       0,
-//       0,
-//       canvasWidth,
-//       canvasHeight
-//     );
-//     gameFrame++;
-//   }
-//   stagger++;
-
-//   requestAnimationFrame(animate);
-// }
-// animate(); // Initial player state is running
-
-// function jump(e) {
-//   if (e.keyCode === 70) {
-//     if (playerState === "run") {
-//       playerState = "attack";
-//       setTimeout(function () {
-//         playerState = "run";
-//       }, 500);
-//     }
-//   }
-// }
-
-// // Add an event listener to trigger the jump function when 'F' key is pressed
-// document.addEventListener("keydown", jump);
+const enemyAttack = () => {};
