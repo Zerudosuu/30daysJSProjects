bf4ff96956c82049bd03a4df8a5ef07b23715035
Ronald Salvador <Zerudosuu@users.noreply.github.com>
2023-10-10 11:36:58 +0800

    Data Extracted or Fetched

--- eCommerce/src/utils/gettingData.js
+++ eCommerce/src/utils/gettingData.js
@@ -7,21 +7,32 @@ const fetchingData = () => {
 
       for (let i = 0; i < response.length; i++) {
         product.innerHTML += `<div class="product__content">
-                <img src="${response[i].image[0]}" alt="" class="product_img">
-                <h2 class="product__title">${response[i].title}</h2>
-                <h4 class="product__category">${response[i].category}</h4>
-                <p class="product__description">${response[i].description}</p>
+                <img src="${response[i].image}" alt="" class="product_img">
+                <h2 class="product__title">${
+                  response[i].title.length > 50
+                    ? response[i].title.substring(0, 50).concat("...")
+                    : response[i].title
+                }</h2>
+                <p class="product__description">${
+                  response[i].description.length > 80
+                    ? response[i].description
+                        .substring(0, 80)
+                        .concat(" ...more")
+                    : response[i].description
+                }</p>
                   <div class="product__price-container">
-                    <h3 class="product__price">${response[i].price}</h3>
-                    <a href="" class="product__addToCart"></a>
+                    <h3 class="product__price"> $${response[i].price}</h3>
+                    <a href="" class="product__addToCart">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+</svg>  
+</a>
                   </div>
         </div>`;
       }
     }
 
-    fetchProducts(
-      "https://https://api.escuelajs.co/api/v1/products.com/products"
-    );
+    fetchProducts("https://fakestoreapi.com/products");
 
     console.log(reponse.error);
   });
