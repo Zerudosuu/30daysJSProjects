b7692e891c6cfea90d95853e742f2d4e031314c1
Ronald Salvador <140569363+Zerudosuu@users.noreply.github.com>
2023-11-11 21:49:50 +0800

    updating shopping section

--- ShoppingCentre/ShoppingCentre/src/main.js
+++ ShoppingCentre/ShoppingCentre/src/main.js
@@ -1,6 +1,9 @@
 import "../Style/modern-normalize.css";
 import "../Style/style.css";
 import "../Style/components/header.css";
+import "../Style/components/hero.css";
+import "../Style/components/brands.css";
+import "../Style/components/shopping.css";
 import "../Style/utils.css";
 
 import fetchingData from "./utils/Data";
--- ShoppingCentre/ShoppingCentre/src/utils/Data.js
+++ ShoppingCentre/ShoppingCentre/src/utils/Data.js
@@ -1,8 +1,9 @@
 let fetcheddata;
+const productContainer = document.querySelector(".productContainer");
 
 const fetchingData = () => {
   document.addEventListener("DOMContentLoaded", function () {
-    fetch("https://api.escuelajs.co/api/v1/products")
+    fetch("https://fakestoreapi.com/products")
       .then((response) => {
         if (!response.ok) {
           throw new Error("Network response was not ok");
@@ -13,16 +14,41 @@ const fetchingData = () => {
         // Work with the fetched data
 
         fetcheddata = structuredClone(data);
+
+        for (var key in fetcheddata) {
+          let title = fetcheddata[key].title;
+          productContainer.innerHTML += ` <div class="product">
+          <img src="${fetcheddata[key].image}" alt="" class="productImage">
+           <h2 class="productTitle">${
+             title.length > 18 ? title.substring(0, 30).concat("...") : title
+           }</h2>
+            <div class="productPriceRating"> 
+              <h2 class="productPrice">$${fetcheddata[key].price}</h2>
+              <h3 class="productRating">Rating: ${
+                fetcheddata[key].rating.rate
+              }</h3>
+            </div>
+         
+            <div class="priceButtonContainer">
+            <button class="btn">Add to cart</button>
+            <button class="btn">Buy</button>
+            </div>
+        </div>`;
+        }
       })
       .catch((error) => {
         // Handle any errors during the fetch
         console.error("There was a problem with the fetch operation:", error);
       });
-  });
 
-  const alreadyfetched = () => {
-    console.log(fetcheddata);
-  };
+    ScrollReveal().reveal(".product", {
+      duration: 1000,
+      origin: "bottom",
+      distance: "50px",
+      easing: "ease-in-out",
+      mobile: false,
+    });
+  });
 };
 
 export default fetchingData;
